57333b7664dfb3906abdae3302e3dad7bc1eb891
Antony <2023sl93002@wilp.bits-pilani.ac.in>
2024-06-01 02:54:26 +0530

    add another test

--- src/test/java/com/qascript/SecondTest.java
+++ src/test/java/com/qascript/SecondTest.java
@@ -18,6 +18,6 @@ public class SecondTest {
         driver = new ChromeDriver(options);
         driver.get("http://34.206.63.71:3000/");
         String actualText = driver.findElement(By.cssSelector("h2.text-2xl.font-bold.mb-4")).getText();
-        Assert.assertTrue("Register text not found", actualText.contains("Register"));
+        Assert.assertTrue("Register text not found", actualText.contains("123"));
     }
 }
